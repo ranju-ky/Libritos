a8afabbdd740f8e0ad666092526f6255a9c87e05
Ranju- <ranjuprj98@gmail.com>
2021-06-14 23:51:25 +0200

    cambio de efecto

--- src/main/java/org/factoriaf5/libritos/controller/BookController.java
+++ src/main/java/org/factoriaf5/libritos/controller/BookController.java
@@ -4,13 +4,12 @@ import org.factoriaf5.libritos.services.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Controller
+@RequestMapping("/books")
 public class BookController {
 
     private BookService bookService;
@@ -40,6 +39,20 @@ public class BookController {
    @PostMapping("/books/new")
    String addBook(@ModelAttribute Book book) {
         bookService.save(book);
+        return "redirect:/boo";
+    }
+
+    @GetMapping("/edit/{id}")
+    String editBook(Model model, @PathVariable Long id){
+        Book book = bookService.findById(id);
+        model.addAttribute("book", book);
+        model.addAttribute("title", "Edit book");
+        return "books/new";
+    }
+
+    @GetMapping("/delete/{id}")
+    String removeBook(@PathVariable Long id){
+        bookService.delete(id);
         return "redirect:/books";
     }
 }
--- src/main/java/org/factoriaf5/libritos/services/BookService.java
+++ src/main/java/org/factoriaf5/libritos/services/BookService.java
@@ -22,6 +22,7 @@ public class BookService {
         return allBooks;
     }
 
+
     public void save(Book book) {
         bookRepository.save(book);
     }
